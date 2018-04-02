e23ef46dc7b12f87ce03771a3ffa857cb30e7955
Kira Liubimova <kira.lyubimova@gmail.com>
2018-04-02 22:51:38 +0300

    Лекция 6.1

---
+++ addressbook_web_tests/addressbook-test-data-generators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAddressbookTests;
+
+namespace addressbook_test_data_generators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestBase.GenerateRandomString(10);
+        }
+    }
+}
